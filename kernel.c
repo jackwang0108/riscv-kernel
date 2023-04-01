8c0599c68e1f3d51080216df8d06d49c4edc0862
Jack Wang <jack4shihong@gmail.com>
2023-04-02 00:46:13 +0800

    test printf and memory

--- kernel.c
+++ kernel.c
@@ -1,11 +1,15 @@
+#include "os.h"
+
 extern void uart_init(void);
-extern void uart_puts(char *s);
+extern void page_init(void);
 
 void start_kernel(void){
 
     // init uart
     uart_init();
     uart_puts("Hello JackOS-riscv!\n");
+
+    page_init();
 
     while (1);
 }
